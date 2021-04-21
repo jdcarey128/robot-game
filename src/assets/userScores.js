b98828187dbbb96d66d82aee9901488a3ab58200
Joshua Carey <jdcarey128@gmail.com>
2021-04-21 15:10:14 -0600

    add user scores to fixture

---
+++ src/assets/userScores.js
@@ -0,0 +1,37 @@
+export default [
+  {
+    "name": "happyDancer582",
+    "score": 15,
+    "created": '2021-04-21T19:58:58.564Z'
+  },
+  {
+    "name": "Gringot$Goblin",
+    "score": 12,
+    "created": '2021-04-20T19:58:58.564Z'
+  },
+  {
+    "name": "Gringot$Goblin",
+    "score": 10,
+    "created": '2021-04-15T19:58:58.564Z'
+  },
+  {
+    "name": "JCDev12",
+    "score": 9,
+    "created": '2021-04-19T19:58:58.564Z'
+  },
+  {
+    "name": "happyDancer582",
+    "score": 8,
+    "created": '2021-04-05T19:58:58.564Z'
+  },
+  {
+    "name": "JCDev12",
+    "score": 8,
+    "created": '2021-04-18T19:58:58.564Z'
+  },
+  {
+    "name": "happyDancer582",
+    "score": 7,
+    "created": '2021-04-13T19:58:58.564Z'
+  },
+]
