5b52c5d24f10fee1093e01d65bd1854adeaf9e1d
Joshua Carey <jdcarey128@gmail.com>
2021-04-21 15:10:02 -0600

    Add app nav bar

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,7 @@
 <template>
+  <nav>
+    <h1 class="title">Robot Grid</h1>
+  </nav>
   <div class="game-wrapper">
     <Game />
   </div>
@@ -24,5 +27,22 @@ export default {
   color: #2c3e50;
   background-color: #F3F5FA;
   min-height: 100vh;
+}
+nav {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: rgba(94, 219, 241, 0.2);
+  width: 100%;
+  border-bottom: 1px solid #ccc;
+  
+  .title {
+    color: #ccc;
+    font-weight: bold;
+    font-size: 40px;
+    padding: 0px 100px;
+    -webkit-text-stroke-width: 0.5px;
+    -webkit-text-stroke-color: rgb(94, 219, 241);
+  }
 }
 </style>
