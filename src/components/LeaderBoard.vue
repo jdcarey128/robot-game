fce81da009f1dfb0bad8b8344dd11f6056729f3a
Joshua Carey <jdcarey128@gmail.com>
2021-04-21 15:10:32 -0600

    Add leader board

--- src/components/LeaderBoard.vue
+++ src/components/LeaderBoard.vue
@@ -1,13 +1,65 @@
 <template>
   <h2>Leader Board</h2>
+  <div class="leader-board-scores">
+    <table>
+      <thead>
+        <th>Name</th>
+        <th>Score</th>
+        <th>Date Set</th>
+      </thead>
+      <tbody>
+        <tr v-for="(user, index) in state.playerLeaders" :key="index">
+          <td>{{user.name}}</td>
+          <td>{{user.score}}</td>
+          <td>{{formatDate(user.created)}}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
 
 <script>
-export default {
+import { reactive } from 'vue'
+import userScores from '@/assets/userScores.js'
 
+export default {
+  name: 'LeaderBoard',
+  setup () {
+    const state = reactive({
+      playerLeaders: userScores
+    })
+
+    function formatDate (date) {
+      return new Date(date).toLocaleDateString()
+    }
+
+    return {
+      state,
+      formatDate
+    }
+  }
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+.leader-board-scores {
+  display: flex;
+  justify-content: center;
+  padding-bottom: 50px;
+}
+table {
+  border-collapse: collapse;
+  width: 50%;
+  border: 1px solid #ccc;
+  padding: 10px;
+}
+th {
+  @extend table;
+  background-color: rgba(94, 219, 241, 0.2);
+}
+
+td {
+  @extend table;
+}
 
 </style>
