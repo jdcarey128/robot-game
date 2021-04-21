6f3451937a9301d090a8b612086aa075afa5ac4c
Joshua Carey <jdcarey128@gmail.com>
2021-04-21 10:19:40 -0600

    Add target image to grid

--- src/components/Square.vue
+++ src/components/Square.vue
@@ -1,20 +1,28 @@
 <template>
-  <div v-bind:class="{'square':(!robotIsPresent),'square-robot':(robotIsPresent), 'square-target':(targetIsPresent)}" @click='coordinates'>
+  <div :class="{
+    'square':(!robotIsPresent),'square-robot':(robotIsPresent),'square-target':(targetIsPresent)
+    }" 
+    @click='coordinates'
+  >
     <div :class="`robot-direction_${robotDirection}`" v-if='robotIsPresent'>
       <img class="robot-image" :src="robotImage" alt="robot">
     </div>
-    <p v-if='targetIsPresent'>T</p>
+    <div class="square-target" v-if='targetIsPresent && !robotIsPresent'>
+      <img class="target-image" :src="targetImage" alt="target">
+    </div>
   </div>
 </template>
 
 <script>
 import robotImage from '@/assets/floating-robot.png'
+import targetImage from '@/assets/target-battery.png'
 
 export default {
   name: 'Square',
   setup () {
     return {
-      robotImage
+      robotImage,
+      targetImage
     }
   },
   props: {
@@ -36,6 +44,9 @@ export default {
 
 <style lang="scss" scoped>
 .square {
+  display: flex;
+  align-items: center;
+  justify-content: center;
   float: left;
   height: 50px;
   width: 50px;
@@ -44,7 +55,6 @@ export default {
   background-color: white;
   border: 1px solid #2c3e50;
   padding: 0px;
-  text-align: center;
   font-size: 30px;
   line-height: 0px;
   font-weight: bold;
@@ -54,13 +64,18 @@ export default {
   @extend .square;
 }
 
+.square-target {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
 .robot-image {
   height: 50px;
 }
 
-.square-target {
-  @extend .square;
-  background-color: lightyellow;
+.target-image {
+  width: 60%;
 }
 
 .robot-direction_Up {
